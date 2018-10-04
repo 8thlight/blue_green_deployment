fcf98c1681aa7bcd01b1ce57faf2668239c611d5
Eric Smith <esmith@8thlight.com>
2018-10-04 17:22:40 -0500

    Configure initial deployment to ec2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "blue_green_deployment_demo"
+set :repo_url, "git@github.com:8thlight/blue_green_deployment.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+server "ec2-13-59-136-67.us-east-2.compute.amazonaws.com", user: "deploy", roles: %w{app db web}
 
 
 
